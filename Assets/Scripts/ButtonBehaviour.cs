ba20cc7c207a11f95a3c5b7082561f7464fd58eb
Unknown <matthew.crees@ntlworld.com>
2020-06-22 20:27:49 +0100

    One-press button

---
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    Animator animator;
+    public bool isPressed;
+
+    private void Start() {
+        animator = GetComponentInParent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        animator.SetTrigger("Pressed");
+        isPressed = true;
+    }
+
+
+}
