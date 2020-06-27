927f3a133ccbc2de08cd820ad995697769cf994e
Unknown <matthew.crees@ntlworld.com>
2020-06-27 14:04:42 +0100

    Doors

--- Assets/Scripts/ButtonBehaviour.cs
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -7,9 +7,16 @@ public class ButtonBehaviour : MonoBehaviour
 {
     Animator animator;
     public bool isPressed;
+    public DoorBehaviour[] doors;
 
     private void Start() {
         animator = GetComponentInParent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        foreach (DoorBehaviour door in doors) {
+            door.open();
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision) {
@@ -20,6 +27,9 @@ public class ButtonBehaviour : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision) {
         isPressed = false;
         animator.SetBool("isPressed", false);
+        foreach (DoorBehaviour door in doors) {
+            door.close();
+        }
     }
 
 }
---
+++ Assets/Scripts/DoorBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour
+{
+    Animator animator;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    public void open() {
+        animator.SetBool("isOpen", true);
+    }
+
+    public void close() {
+        animator.SetBool("isOpen", false);
+    }
+
+}
