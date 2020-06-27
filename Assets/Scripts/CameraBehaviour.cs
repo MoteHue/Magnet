565e754fa52c3d0c53fff98f237199a6c9d56c77
Unknown <matthew.crees@ntlworld.com>
2020-06-27 12:30:23 +0100

    camera size change

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,19 +7,26 @@ public class CameraBehaviour : MonoBehaviour
 {
     public float cameraSmoothSpeed = 0.1f;
     Vector3 targetPosition;
+    float targetSize;
 
     private void Start() {
-        targetPosition = new Vector3(0f, 0f, -10f);
+        targetPosition = new Vector3(-1.9f, 0f, -10f);
+        targetSize = 13.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = Vector3.Lerp(transform.position, targetPosition, cameraSmoothSpeed);
+        GetComponent<Camera>().orthographicSize = Mathf.Lerp(GetComponent<Camera>().orthographicSize, targetSize, cameraSmoothSpeed);
     }
 
     public void SetTargetPosition (Vector3 target) {
         targetPosition = target;
+    }
+
+    public void SetTargetSize(float target) {
+        targetSize = target;
     }
 
 }
--- Assets/Scripts/CameraTriggerBehaviour.cs
+++ Assets/Scripts/CameraTriggerBehaviour.cs
@@ -7,12 +7,14 @@ public class CameraTriggerBehaviour : MonoBehaviour
 {
     public Camera mainCamera;
     public Vector3 newCameraPosition;
+    public float newCameraSize;
     public GameObject alternateTrigger;
     public Light2D[] lightsToTurnOff;
     public Light2D[] lightsToTurnOn;
 
     private void OnTriggerEnter2D(Collider2D collision) {
         mainCamera.GetComponent<CameraBehaviour>().SetTargetPosition(newCameraPosition);
+        mainCamera.GetComponent<CameraBehaviour>().SetTargetSize(newCameraSize);
         foreach (Light2D light in lightsToTurnOff) {
             light.intensity = 0f;
         }
